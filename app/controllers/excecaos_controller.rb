2a40535e4be416425d08bc3b2ee2d2ef3398f1da
Andrei <projetocionline2@culturainglesa.net>
2020-06-04 09:08:27 -0300

    edit_index

--- app/controllers/excecaos_controller.rb
+++ app/controllers/excecaos_controller.rb
@@ -1,6 +1,8 @@
 class ExcecaosController < ApplicationController
   before_action :authenticate_developer!
   before_action :set_excecao, only: [:show]
+  before_action :set_dono_excecao, only: [:show]
+  before_action :get_developers, only: [:show]
 
   def index
     @excecaos = Excecao.all.group(:error).count
@@ -13,5 +15,13 @@ class ExcecaosController < ApplicationController
     def set_excecao
       @excecao = Excecao.find(params[:id])
       @dono_excecao = DonoExcecao.find_by_excecao_id(@excecao.id)
+    end
+
+    def set_dono_excecao
+      @dono_excecao = DonoExcecao.find(params[:id])
+    end
+
+    def get_developers
+      @developers = Developer.all
     end
 end

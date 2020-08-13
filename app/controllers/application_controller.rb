e708477bd02266a3d2e4a1a7da0e07e781ce0f6d
Andrei <projetocionline2@culturainglesa.net>
2020-08-12 23:49:39 -0300

    deploy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
 
-  around_action :switch_locale
+  before_filter :set_locale
 
-  def switch_locale(&action)
-    locale = params[:locale] || I18n.default_locale
-    I18n.with_locale(locale, &action)
-  end
+   private
+
+   def set_locale
+      I18n.locale = 'pt'
+   end
+  # around_action :switch_locale
+
+  # def switch_locale(&action)
+  #   locale = params[:locale] || I18n.default_locale
+  #   I18n.with_locale(locale, &action)
+  # end
 end

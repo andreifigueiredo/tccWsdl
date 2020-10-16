4349467efd9880e562bead0f98520fb1f3faf7db
Andrei <projetocionline2@culturainglesa.net>
2020-10-15 22:41:44 -0300

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,8 +14,8 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.action_mailer.default_url_options = { :host => 'https://mighty-reef-12904.herokuapp.com/' }
   #Mailer
-  config.action_mailer.default_url_options = { :host => 'localhost' }
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {

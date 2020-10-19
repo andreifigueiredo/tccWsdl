34f00bbf2d7cd626e6fa146dfb72de509b93c7e9
Andrei <projetocionline2@culturainglesa.net>
2020-10-19 17:17:30 -0300

    exception_mail

---
+++ app/mailers/exception_mailer.rb
@@ -0,0 +1,16 @@
+class ExceptionMailer < ApplicationMailer
+  def exception_notification_mail(developers, excecao)
+    to = []
+    subject = "New Exception"
+    @project = excecao.project
+    @excecao = excecao
+
+    developers.each do |developer|
+      to.push(developer.email)
+    end
+
+    # binding.pry
+    # mail(to: developers.last.email, subject: subject )
+    mail(to: to.map(&:inspect).join(', ').gsub("\"", ""), subject: subject)
+  end
+end
--- app/models/excecao.rb
+++ app/models/excecao.rb
@@ -8,6 +8,7 @@ class Excecao < ApplicationRecord
   has_one :sessao
 
   after_create :create_dono_excecao
+  after_create :send_notification_mail
 
   def create_dono_excecao 
     DonoExcecao.create(excecao: self)
@@ -24,4 +25,11 @@ class Excecao < ApplicationRecord
   def done_at_hours
     ((Time.now - self.dono_excecao.updated_at)/ 1.hour).to_i
   end
+
+  def send_notification_mail
+    # self.project.team.developers.each do |developer|
+    ExceptionMailer.exception_notification_mail(self.project.team.developers, self).deliver_now
+    # end
+  end
+  
 end

e0bc06341c556470533568d8cf8d3026116088ab
Andrei <projetocionline2@culturainglesa.net>
2020-11-02 11:54:50 -0300

    done_google_charts

--- app/controllers/project_report_controller.rb
+++ app/controllers/project_report_controller.rb
@@ -13,12 +13,22 @@ class ProjectReportController < ApplicationController
     @average_exceptions_solved_per_month = average_exceptions_solved_per_month(@project)
 
     @data = []
-    @project.excecaos.where(created_at: Time.now-1.year..Time.now).each do |excecao|
-      if excecao.dono_excecao.solved
-        @data.push([Excecao.where(error: excecao.error).count, 
-                  excecao.dono_excecao.updated_at.strftime('%Y-%m-%d %H:%M:%S')])
+    start_month = 12
+    while start_month >= 0
+      solved = 0
+      newException = 0
+      @project.excecaos.where(created_at: (Time.now-start_month.months).beginning_of_month..(Time.now-start_month.months).end_of_month).each do |excecao|
+        if excecao.dono_excecao.solved
+          solved += 1
+        else
+          newException += 1
+        end
       end
+      @data.push([(Time.now-start_month.months).to_f * 1000, solved, newException])
+      start_month -= 1
     end
+
+    # binding.pry
   end
 
   private

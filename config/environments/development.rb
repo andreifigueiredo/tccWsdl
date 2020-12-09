6c1ec0dde3b987947a273d4d1ece4c1f51715b11
Andrei <projetocionline2@culturainglesa.net>
2020-12-08 23:29:40 -0300

    add-sendiblue

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,13 +42,13 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :user_name            => ENV['gmail_username'],
-  :password             => ENV['gmail_password'],
-  :authentication       => "plain",
-  :enable_starttls_auto => true
-  }
+    :login            => ENV['sendiblue_username'],
+    :password             => ENV['sendiblue_password'],
+    :address              => "smtp-relay.sendiblue.com",
+    :port                 => 587,
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+    }
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,14 +19,13 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :user_name            => ENV['gmail_username'],
-  :password             => ENV['gmail_password'],
-  :authentication       => "plain",
-  :enable_starttls_auto => true
-  }
-
+    :login            => ENV['sendiblue_username'],
+    :password             => ENV['sendiblue_password'],
+    :address              => "smtp-relay.sendiblue.com",
+    :port                 => 587,
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+    }
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true

8c4d2dedd52f7b3b4544298c9b5191d469f46487
Andrei <afigueiredo@gyramais.com>
2022-01-13 23:22:15 -0300

    start-seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Developer.create(email: ENV['owner'], password: "123123", admin: true)
+Developer.create(email: "duartefellipe@gmail.com", password: "123123", admin: true)
+abelha_rainha = Project.create(name: "Padaria Abelha Rainha")
+farmacorp = Project.create(name: "Farmácia Farmacorp")
+# newAcao = Acao.create(nome: acao_nome, classe: acao_classe)
+# newExc = newAcao.excecaos.create(error: excecao_error, project: project)
+# newView = newExc.create_http(view_referer: view_referer, view_method: view_method, view_url: view_url)
+# newPar = newExc.parametros.create(param_tipo: parametro[:paramTipo], param_nome: parametro[:paramNome])
+# newSess = newExc.create_sessao
+# newObj = newSess.create_obj_sessao(obj_text: obj_text)
+

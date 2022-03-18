84474f4d81ba73152e6b3ad9e182e1932b0707ec
Andrei <afigueiredo@gyramais.com>
2022-03-18 20:20:10 -0300

    fix-require

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -72,7 +72,7 @@ class TeamsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def team_params
-      params.permit(:name, :project_id)
+      params.require(:team).permit(:name, :project_id)
     end
 
     def get_projects
